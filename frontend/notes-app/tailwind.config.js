436f111aa7ca07b0cf17552918688555ab5df22a
Arsen Kohut <kohut.arsen@gmail.com>
2024-05-07 20:53:13 +0300

    Create login page. Add PasswordInput component and helper to validate email

--- frontend/notes-app/tailwind.config.js
+++ frontend/notes-app/tailwind.config.js
@@ -2,7 +2,13 @@
 export default {
   content: ["./src/**/*.{html,js,jsx}"],
   theme: {
-    extend: {},
+    extend: {
+      // Colors used in the project
+      colors: {
+        primary: "#2B85FF",
+        secondary: "#EF863E",
+      },
+    },
   },
   plugins: [],
 }
