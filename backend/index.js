033311ef939c0fb5b0a3d62f5c162352c002f5be
Arsen Kohut <kohut.arsen@gmail.com>
2024-07-05 14:00:39 +0300

    Add POST method for 'create account API'

--- backend/index.js
+++ backend/index.js
@@ -5,6 +5,8 @@ const mongoose = require("mongoose");
 
 mongoose.connect(config.connectionString);
 
+const User = require("./models/user.model");
+
 const express = require('express');
 const cors = require('cors');
 const app = express();
@@ -24,7 +26,57 @@ app.get("/", (req, res) => {
     res.json({ data: 'hello' });
 });
 
+// Create account API
+app.post("/create-account", async (req, res) => {
+    const { fullName, email, password } = req.body;
 
+    if (!fullName) {
+        res
+            .status(400)
+            .json({ error: true, message: "Full Name is required"});
+        return;
+    }
+    if (!email) {
+        res
+            .status(400)
+            .json({ error: true, message: "Email is required"});
+        return;
+    }
+    if(!password) {
+        res
+            .status(400)
+            .json({ error: true, message: "Password is required"});
+        return;
+    }
+
+    const isUser = await User.findOne({ email: email });
+
+    if (isUser) {
+        return res.json({
+            error: true,
+            message: "User already exist",
+        });
+    }
+
+    const user = new User({
+        fullName,
+        email,
+        password,
+    });
+
+    await user.save();
+
+    const accessToken = jwt.sign({ user }, proccess.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: "36000m",
+    });
+
+    return res.json({
+        error: false,
+        user,
+        accessToken,
+        message: "Registation Successful",
+    });
+});
 
 app.listen(8000);
 
