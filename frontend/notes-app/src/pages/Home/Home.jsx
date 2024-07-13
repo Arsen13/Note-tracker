99e3b4ee81bf4de2fe4ed5f8de4a6452c37479d0
Arsen Kohut <kohut.arsen@gmail.com>
2024-07-13 17:12:56 +0300

    Add 'get-all-notes API' integration

--- frontend/notes-app/src/pages/Home/Home.jsx
+++ frontend/notes-app/src/pages/Home/Home.jsx
@@ -15,6 +15,7 @@ const Home = () => {
         data: null,
     });
 
+    const [allNotes, setAllNotes] = useState([]);
     const [userInfo, setUserInfo] = useState(null);
 
     const navigate = useNavigate();
@@ -34,10 +35,24 @@ const Home = () => {
         }
     };
 
+    // Get all notes
+    const getAllNotes = async () => {
+        try {
+            const response = await axiosInstance("/get-all-notes");
+
+            if (response.data && response.data.notes) {
+                setAllNotes(response.data.notes);
+            }
+        } catch (error) {
+            console.log("An unexpected error occured. Please try again");
+        }
+    }
+
     useEffect(() => {
+        getAllNotes();
         getUserInfo();
         return () => {};
-    }, []);
+    }, [allNotes]);
 
     return (
         <>
@@ -45,16 +60,19 @@ const Home = () => {
 
             <div className="container mx-auto">
                 <div className="grid grid-cols-3 gap-4 mt-8">
-                    <NoteCard 
-                        title="Meeting on 7th April"
-                        date='3rd Apr 2024'
-                        content="Meeting on 7th April"
-                        tags="#Meeting"
-                        isPinned={true}
-                        onEdit={()=>{}}
-                        onDelete={()=>{}}
-                        onPinNote={()=>{}}
-                    />
+                    {allNotes.map((item, index) => (
+                        <NoteCard 
+                            key={item._id}
+                            title={item.title}
+                            date={item.createdOn}
+                            content={item.content}
+                            tags={item.tags}
+                            isPinned={true}
+                            onEdit={()=>{}}
+                            onDelete={()=>{}}
+                            onPinNote={()=>{}}
+                        />
+                    ))}
                 </div>
             </div>
 
